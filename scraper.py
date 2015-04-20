3a3b6559fbfd6073fdbaae56ced755a6d4fe2fd0
woodbine <ian@spendnetwork.com>
2015-04-20 22:11:59 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -29,8 +29,9 @@ links = block.findAll('a', href=True)
 for link in links:
 	url = 'http://www.cambridgeshire.gov.uk' + link['href']
 	if 'download/downloads/id' in url:
-		title = link.contents[0]
+		#title = link.contents[0]
   		title = link.encode_contents(formatter='html').replace('&nbsp;',' ').strip() #  gets rid of erroneous &nbsp; chars
+		#title = title
 		if '.csv' in title:
 			# create the right strings for the new filename
 			csvYr = title.split(' ')[-1][:4]
